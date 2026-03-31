d66ca40211f456d7e0b1ba92f85b4ea0be02a681
gimattiolo <gimattiolo@gmail.com>
2026-03-31 16:18:50 -0700

    added focus and undistort helpers

--- CalibrationUtilities.py
+++ CalibrationUtilities.py
@@ -328,3 +328,79 @@ def invertExtrisics(M) :
 
     return invM
 
+class Focuser:
+    bus = None
+    CHIP_I2C_ADDR = 0x0C
+
+    def __init__(self, bus):
+        self.focus_value = 0
+        self.bus = bus
+        pass
+        
+    def read(self):
+        return self.focus_value
+
+    def write(self, chip_addr, value):
+        if value < 0:
+            value = 0
+        self.focus_value = value
+
+        value = (value << 4) & 0x3ff0
+        data1 = (value >> 8) & 0x3f
+        data2 = value & 0xf0
+        os.system("i2cset -y {} 0x{:02X} {} {}".format(self.bus, chip_addr, data1, data2))
+
+    OPT_BASE    = 0x1000
+    OPT_FOCUS   = OPT_BASE | 0x01
+    OPT_ZOOM    = OPT_BASE | 0x02
+    OPT_MOTOR_X = OPT_BASE | 0x03
+    OPT_MOTOR_Y = OPT_BASE | 0x04
+    OPT_IRCUT   = OPT_BASE | 0x05
+    opts = {
+        OPT_FOCUS : {
+            "MIN_VALUE": 0,
+            "MAX_VALUE": 1000,
+            "DEF_VALUE": 0,
+        },
+    }
+    def reset(self,opt,flag = 1):
+        info = self.opts[opt]
+        if info == None or info["DEF_VALUE"] == None:
+            return
+        self.set(opt,info["DEF_VALUE"])
+
+    def get(self,opt,flag = 0):
+        info = self.opts[opt]
+        return self.read()
+
+    def set(self,opt,value,flag = 1):
+        info = self.opts[opt]
+        if value > info["MAX_VALUE"]:
+            value = info["MAX_VALUE"]
+        elif value < info["MIN_VALUE"]:
+            value = info["MIN_VALUE"]
+        self.write(self.CHIP_I2C_ADDR, value)
+        print("write: {}".format(value))
+
+def ComputeUndistortRectifyMap(fisheye, h, w, camera_matrix, distortion_coefficients) :
+    R = None
+    undistorted_image_size = (w,h)
+    #m1type Type of the first output map, e.g., cv2.CV_16SC2 or cv2.CV_32F    
+    ml_type=cv2.CV_16SC2
+    if fisheye :
+        new_camera_matrix = camera_matrix.copy()
+        mapx, mapy = cv2.fisheye.initUndistortRectifyMap(camera_matrix, distortion_coefficients, R, new_camera_matrix, undistorted_image_size, m1type=ml_type)
+        roi = (0, 0, w, h)
+    else :
+        # Refine the camera matrix (optional, as above)
+        new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(camera_matrix, distortion_coefficients, imageSize=undistorted_image_size, alpha=1, newImageSize=undistorted_image_size, centerPrincipalPoint=False)
+        # Compute the undistortion and rectification transformation maps once
+        mapx, mapy = cv2.initUndistortRectifyMap(camera_matrix, distortion_coefficients, R, new_camera_matrix, undistorted_image_size, m1type=ml_type)
+    return new_camera_matrix, roi, mapx, mapy
+
+def UndistortImage(img, mapx, mapy) :
+    return cv2.remap(img, mapx, mapy, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+
+def CropUndistortedImage(img, roi) :
+    x, y, w, h = roi
+    return img[y:y+h, x:x+w]
